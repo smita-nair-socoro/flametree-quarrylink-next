18be77b3113337a0e0fd8a02044d0ad368b76a86
Reza Bahrami <reza.bahrami@socoro.com.au>
2025-07-03 21:42:23 +1000

    feat(form): added scrollArea to form

--- app/(protected)/inventory/products/(components)/product-form.tsx
+++ app/(protected)/inventory/products/(components)/product-form.tsx
@@ -17,10 +17,22 @@ import { useForm } from 'react-hook-form';
 import z from 'zod';
 import { Textarea } from '@/components/ui/textarea';
 import { MultiSelect } from '@/components/ui/multi-select';
-import { Separator } from "@/components/ui/separator"
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
+import { Separator } from '@/components/ui/separator';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
 import { Check, ChevronsUpDown } from 'lucide-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function ProductForm({
   className,
@@ -30,14 +42,14 @@ export default function ProductForm({
       .string()
       .min(2, { message: 'Product Name must be at least 2 characters.' })
       .max(100, { message: "Product Name can't be more than 100 characters" }),
-    product_code: z
-      .string().optional(),
+    product_code: z.string().optional(),
     category: z
       .array(z.string())
       .min(1, { message: 'Select at least one category.' }),
-    description: z
-      .string().optional(),
-    quarry_sources: z.string().min(2, { message: "Please select at least one quarry" }),
+    description: z.string().optional(),
+    quarry_sources: z
+      .string()
+      .min(2, { message: 'Please select at least one quarry' }),
     cost_price_per_tonne: z.string(),
     sell_price_per_tonne: z.string(),
   });
@@ -61,218 +73,241 @@ export default function ProductForm({
     console.log(values);
   }
 
-
   //TODO: Fetch this from api call for now hard code it
   const categoryList = [
-    { value: "react", label: "React" },
-    { value: "angular", label: "Angular" },
-    { value: "vue", label: "Vue" },
-    { value: "svelte", label: "Svelte" },
-    { value: "ember", label: "Ember" },
+    { value: 'react', label: 'React' },
+    { value: 'angular', label: 'Angular' },
+    { value: 'vue', label: 'Vue' },
+    { value: 'svelte', label: 'Svelte' },
+    { value: 'ember', label: 'Ember' },
   ];
 
   //TODO: Fetch this from api call as well
   const languages = [
-    { label: "English", value: "en" },
-    { label: "French", value: "fr" },
-    { label: "German", value: "de" },
-    { label: "Spanish", value: "es" },
-    { label: "Portuguese", value: "pt" },
-    { label: "Russian", value: "ru" },
-    { label: "Japanese", value: "ja" },
-    { label: "Korean", value: "ko" },
-    { label: "Chinese", value: "zh" },
-  ] as const
+    { label: 'English', value: 'en' },
+    { label: 'French', value: 'fr' },
+    { label: 'German', value: 'de' },
+    { label: 'Spanish', value: 'es' },
+    { label: 'Portuguese', value: 'pt' },
+    { label: 'Russian', value: 'ru' },
+    { label: 'Japanese', value: 'ja' },
+    { label: 'Korean', value: 'ko' },
+    { label: 'Chinese', value: 'zh' },
+  ] as const;
 
   return (
-    <Form {...form}>
-      <form
-        className={cn('grid grid-cols-1 md:grid-cols-2 items-start gap-6', className)}
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <div className="grid gap-3">
-          <FormField
-            control={form.control}
-            name="product_name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Product Name*</FormLabel>
-                <FormControl>
-                  <Input className="w-full" placeholder="product name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+    <ScrollArea className="overflow-auto">
+      <Form {...form}>
+        <form
+          className={cn(
+            'grid grid-cols-1 md:grid-cols-2 items-start gap-6',
+            className,
+          )}
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <div className="grid col-span-2 md:col-span-1 gap-3">
+            <FormField
+              control={form.control}
+              name="product_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Name*</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="w-full"
+                      placeholder="product name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <div className='grid gap-3'>
-          <FormField
-            control={form.control}
-            name="product_code"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Product Code</FormLabel>
-                <FormControl>
-                  <Input className='w-full' placeholder="ION001" {...field} />
-                </FormControl>
-                <FormDescription>Optional unique identifier for this product</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+          <div className="grid col-span-2 md:col-span-1 gap-3">
+            <FormField
+              control={form.control}
+              name="product_code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Code</FormLabel>
+                  <FormControl>
+                    <Input className="w-full" placeholder="ION001" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Optional unique identifier for this product
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <div className="md:col-span-2 grid gap-3">
-          <FormField
-            control={form.control}
-            name="category"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Categories*</FormLabel>
-                <FormControl>
-                  <MultiSelect
-                    options={categoryList}
-                    className='w-full'
-                    placeholder="Pick categories…"
-                    defaultValue={field.value}
-                    onValueChange={field.onChange}
-                    maxCount={3}
-                  />
-                </FormControl>
-                <FormDescription>Select one or more categories</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+          <div className="col-span-2 grid gap-3">
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Categories*</FormLabel>
+                  <FormControl>
+                    <MultiSelect
+                      options={categoryList}
+                      className="w-full"
+                      placeholder="Pick categories…"
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                      maxCount={3}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Select one or more categories
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <div className="md:col-span-2 grid gap-3">
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Textarea placeholder="Product description"
-                    className="resize-none w-full"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+          <div className="col-span-2 grid gap-3">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Product description"
+                      className="resize-none w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-        <Separator className='md:col-span-2' orientation='horizontal' />
+          <Separator className="col-span-2" orientation="horizontal" />
 
-        <h2 className='col-span-2 font-bold'>Quarry Sources</h2>
+          <h2 className="col-span-2 font-bold">Quarry Sources</h2>
 
-        <div className='grid grid-cols-1 md:col-span-2 md:grid-cols-3 gap-3'>
-          <FormField
-            control={form.control}
-            name="quarry_sources"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Quarry</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant="outline"
-                        role="combobox"
-                        className={cn(
-                          "w-full justify-between",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        {field.value
-                          ? languages.find(
-                            (language) => language.value === field.value
-                          )?.label
-                          : "Select Quarry"}
-                        <ChevronsUpDown className="opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput
-                        placeholder="Search Quarry..."
-                        className="h-9"
-                      />
-                      <CommandList>
-                        <CommandEmpty>No Quarry found.</CommandEmpty>
-                        <CommandGroup>
-                          {languages.map((language) => (
-                            <CommandItem
-                              value={language.label}
-                              key={language.value}
-                              onSelect={() => {
-                                form.setValue("quarry_sources", language.value)
-                              }}
-                            >
-                              {language.label}
-                              <Check
-                                className={cn(
-                                  "ml-auto",
-                                  language.value === field.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <div className="grid grid-cols-1 col-span-2 md:grid-cols-3 gap-3">
+            <FormField
+              control={form.control}
+              name="quarry_sources"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Quarry</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            'w-full justify-between',
+                            !field.value && 'text-muted-foreground',
+                          )}
+                        >
+                          {field.value
+                            ? languages.find(
+                                (language) => language.value === field.value,
+                              )?.label
+                            : 'Select Quarry'}
+                          <ChevronsUpDown className="opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandInput
+                          placeholder="Search Quarry..."
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No Quarry found.</CommandEmpty>
+                          <CommandGroup>
+                            {languages.map((language) => (
+                              <CommandItem
+                                value={language.label}
+                                key={language.value}
+                                onSelect={() => {
+                                  form.setValue(
+                                    'quarry_sources',
+                                    language.value,
+                                  );
+                                }}
+                              >
+                                {language.label}
+                                <Check
+                                  className={cn(
+                                    'ml-auto',
+                                    language.value === field.value
+                                      ? 'opacity-100'
+                                      : 'opacity-0',
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
+            <FormField
+              control={form.control}
+              name="cost_price_per_tonne"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Cost Price per tonne</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      className="w-full"
+                      placeholder="$00"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="cost_price_per_tonne"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Cost Price per tonne</FormLabel>
-                <FormControl>
-                  <Input type='number' className='w-full' placeholder="$00" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="sell_price_per_tonne"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Sell Price per tonne</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      className="w-full"
+                      placeholder="$00"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-
-          <FormField
-            control={form.control}
-            name="sell_price_per_tonne"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Sell Price per tonne</FormLabel>
-                <FormControl>
-                  <Input type='number' className='w-full' placeholder="$00" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-        </div>
-
-        <div className="md:col-span-2">
-          <Button className="w-full" type="submit">
-            Save changes
-          </Button>
-        </div>
-      </form>
-    </Form>
+          <div className="col-span-2">
+            <Button className="w-full" type="submit">
+              Save changes
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </ScrollArea>
   );
 }
