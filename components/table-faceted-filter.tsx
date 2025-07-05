833c0b6bc7cc92ea6762a5c606cd24162687e8b4
Reza Bahrami <reza.bahrami@socoro.com.au>
2025-07-05 16:51:02 +1000

    chore(imports): removed unused import

--- components/table-faceted-filter.tsx
+++ components/table-faceted-filter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { Badge } from '@/components/ui/badge';
