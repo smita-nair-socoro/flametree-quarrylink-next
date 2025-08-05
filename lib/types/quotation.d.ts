e029ffc670506fe738c34957d53c41277e815b33
Reza Bahrami <reza.bahrami@socoro.com.au>
2025-08-06 09:34:34 +1000

    feat(quote_items): added quote_items types

--- lib/types/quotation.d.ts
+++ lib/types/quotation.d.ts
@@ -17,6 +17,60 @@ export enum QUOTE_STATUS {
   ARCHIVED = 'ARCHIVED',
 }
 
+export enum QUOTE_ITEM_STATUS {
+  ACTIVE = 'ACTIVE',
+  COMPLETED = 'COMPLETED',
+  CANCELLED = 'CANCELLED',
+}
+
+export enum COST_UNIT {
+  TN = 'TN',
+  M3 = 'M3',
+  KG_20 = 'KG_20',
+  BULKA = 'BULKA',
+}
+
+export enum TRUCK_RATE_TYPE {
+  TN = 'TN',
+  HOURLY = 'HOURLY',
+  M3 = 'M3',
+  LOAD = 'LOAD',
+}
+
+export interface QuoteItem {
+  id: number;
+  quote_id: number;
+  product_id: number;
+  total_quantity_required: number;
+  remaining_quantity: number;
+  override_cost_price_per_tn: number;
+  override_sell_price_per_tn: number;
+  override_cost_price_per_m3: number;
+  override_sell_price_per_m3: number;
+  override_cost_price_per_20kg: number;
+  override_sell_price_per_20kg: number;
+  override_cost_price_per_bulka: number;
+  override_sell_price_per_bulka: number;
+  override_tn_truck_cost_rate: number;
+  override_m3_truck_cost_rate: number;
+  override_hourly_truck_cost_rate: number;
+  override_load_truck_cost_rate: number;
+  override_tn_truck_sell_rate: number;
+  override_m3_truck_sell_rate: number;
+  override_hourly_truck_sell_rate: number;
+  selected_cost_unit: COST_UNIT;
+  selected_sell_unit: COST_UNIT;
+  selected_truck_rate_type: TRUCK_RATE_TYPE;
+  quote_item_status: QUOTE_ITEM_STATUS;
+  notes: string;
+  version: number;
+  is_deleted: boolean;
+  created_by: string;
+  created_at: string;
+  updated_at: string;
+  last_modified_by: string;
+}
+
 export interface Quotation {
   id: number;
   customer_id: number;
@@ -45,6 +99,7 @@ export interface QuotationDetails extends Quotation {
   customer: Customer;
   job: Job;
   quarry_products: QuarriesWithPrice[];
+  quote_items: QuoteItem[];
 }
 
 interface QuotationDetailsResponse {
