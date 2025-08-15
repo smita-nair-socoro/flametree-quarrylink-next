44133510c2e7e6e1bd41b7cd575bb80ca96f2858
Jaywoo Choi <jay@Jaywoos-MacBook-Pro.local>
2025-08-15 13:26:52 +1000

    feat(form): initial draft of form and schemas

--- app/(protected)/customer-operations/customers/page.tsx
+++ app/(protected)/customer-operations/customers/page.tsx
@@ -1,12 +1,21 @@
-export default function CustomerPage() {
+'use client';
+
+export default function CustomersPage() {
+  //TODO: Fetch from server
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
+        <div>
+          <h1 className="text-2xl">Customers</h1>
+        </div>
+        <div className="flex flex-col sm:flex-row sm:items-center gap-1">
+          {/* TODO: Form Goes here */}
+        </div>
       </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+
+      <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+        {/* TODO: Add table here */}
+      </div>
     </div>
   );
 }
