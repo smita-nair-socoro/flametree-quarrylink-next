ac2f5b9ae5b0f35bc973827f1ae70d88dd56db99
unknown <jaywoo.choi@socoro.com.au>
2025-08-21 16:26:26 +1000

    added some customer data

---
+++ lib/utils/customer-helper.ts
@@ -0,0 +1,23 @@
+// Handling customer statuses
+// TODO: combine with quote-helpers.ts to handle all the statuses based on the module
+
+// TODO: QLINK-248
+// import { CUSTOMER_STATUS } from '../types/customer-enums';
+
+// export const formatCustomerStatus = (
+//   status: CUSTOMER_STATUS | string
+// ): string => {
+//   switch (status) {
+//     case CUSTOMER_STATUS.ACTIVE:
+//     case 'ACTIVE':
+//       return 'ACTIVE';
+//     case CUSTOMER_STATUS.INACTIVE:
+//     case 'INACTIVE':
+//       return 'INACTIVE';
+//     case CUSTOMER_STATUS.ARCHIVED:
+//     case 'ARCHIVED':
+//       return 'ARCHIVED';
+//     default:
+//       return status.replace(/_/g, ' ');
+//   }
+// };
