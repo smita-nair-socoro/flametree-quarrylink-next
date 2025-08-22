ed801e668a933d9dcfb3d767112e21fad6197310
Jaywoo Choi <jaywoo.choi@socoro.com.au>
2025-08-22 12:02:25 +1000

    feat(table): added customer main table

--- lib/utils/customer-helper.ts
+++ lib/utils/customer-helper.ts
@@ -1,23 +1,20 @@
 // Handling customer statuses
-// TODO: combine with quote-helpers.ts to handle all the statuses based on the module
+// TODO: Combine with quote-helpers.ts to handle all the statuses based on the module
 
-// TODO: QLINK-248
-// import { CUSTOMER_STATUS } from '../types/customer-enums';
+import { CUSTOMER_STATUS } from '../types/customer-enums';
 
-// export const formatCustomerStatus = (
-//   status: CUSTOMER_STATUS | string
-// ): string => {
-//   switch (status) {
-//     case CUSTOMER_STATUS.ACTIVE:
-//     case 'ACTIVE':
-//       return 'ACTIVE';
-//     case CUSTOMER_STATUS.INACTIVE:
-//     case 'INACTIVE':
-//       return 'INACTIVE';
-//     case CUSTOMER_STATUS.ARCHIVED:
-//     case 'ARCHIVED':
-//       return 'ARCHIVED';
-//     default:
-//       return status.replace(/_/g, ' ');
-//   }
-// };
+export const formatCustomerStatus = (
+  status: CUSTOMER_STATUS | string
+): string => {
+  switch (status) {
+    case CUSTOMER_STATUS.ACTIVE:
+    case 'ACTIVE':
+      return 'ACTIVE';
+
+    case CUSTOMER_STATUS.ARCHIVED:
+    case 'ARCHIVED':
+      return 'ARCHIVED';
+    default:
+      return status.replace(/_/g, ' ');
+  }
+};
