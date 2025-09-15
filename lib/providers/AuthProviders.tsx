d587b060775f5be311310a56b496939a249afbb4
Jaywoo Choi <jaywoo.choi@socoro.com.au>
2025-09-15 17:25:36 +1000

    fixed

--- lib/providers/AuthProviders.tsx
+++ lib/providers/AuthProviders.tsx
@@ -13,7 +13,7 @@ const webStorageStore =
 const oidcConfig = {
   authority: process.env.NEXT_PUBLIC_COGNITO_DOMAIN!,
   client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-  redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI!,
+  redirect_uri: getRedirectUri(),
   response_type: 'code',
   scope: SCOPE,
   stateStore: webStorageStore,
@@ -31,4 +31,19 @@ export function OidcAuthProvider({ children }: { children: ReactNode }) {
 
 export function AppAuthProviders({ children }: { children: ReactNode }) {
   return <OidcAuthProvider>{children}</OidcAuthProvider>;
+}
+
+function getRedirectUri() {
+  if (typeof window !== 'undefined') {
+    const origin = window.location.origin;
+
+    if (origin.includes('app.dev.quarrylink.com.au')) {
+      return `${origin}/callback/`; // dev with trailing slash
+    }
+
+    return `${origin}/callback`; // local or other environments
+  }
+
+  // Fallback to env (useful for SSR/build time)
+  return process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI!;
 }
