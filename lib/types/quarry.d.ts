c8206ac5c06c16a280a7cc46d0d95a5893145522
Charlie Peng <charlie.peng@socoro.com.au>
2025-10-07 11:38:55 +1100

    modify quarry type

--- lib/types/quarry.d.ts
+++ lib/types/quarry.d.ts
@@ -1,16 +1,40 @@
 import { QUARRY_STATUS } from './quarry-enums';
 
+// export interface Quarry {
+//   id: number;
+//   quarry_name: string;
+//   status: QUARRY_STATUS;
+//   version: number;
+//   is_deleted: boolean;
+//   created_by: string;
+//   created_at: string;
+//   updated_at: string;
+//   last_modified_by: string;
+// }
+
 export interface Quarry {
   id: number;
-  quarry_name: string;
-  status: QUARRY_STATUS;
+  name: string;
+  status: QuarryStatus;
+  type: QuarryType;
+  website: string;
+  email: string;
+  phone: string;
+  // address: number; // FK to Address.id - to be implemented
+  contactPersonName: string;
+  contactPersonEmail: string;
+  contactPersonPhone: string;
+  openingClosingTimes: string;
+  // weighbridgeInfo: string;
+  notes: string;
   version: number;
-  is_deleted: boolean;
-  created_by: string;
-  created_at: string;
-  updated_at: string;
-  last_modified_by: string;
+  isDeleted: boolean;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  lastModifiedBy: string;
 }
+
 
 export interface QuarryProductPrice {
   product_id: number;
