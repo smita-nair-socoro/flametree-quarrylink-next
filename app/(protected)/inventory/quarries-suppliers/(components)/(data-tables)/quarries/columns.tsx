962bac0212df6c058a62ace09d8c3d97c66ebf5d
Charlie Peng <charlie.peng@socoro.com.au>
2025-10-07 11:38:19 +1100

    modify quarry coloumn

---
+++ app/(protected)/inventory/quarries-suppliers/(components)/(data-tables)/quarries/columns.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { TableBadges } from '@/components/table-badges';
+import { TableClientSortableHeader } from '@/components/table-client-sortable-header';
+import { Quarry } from '@/lib/types/quarry';
+import { ColumnDef } from '@tanstack/react-table';
+import { QuarryTableAction } from './quarry-table-actions';
+
+export const quarriesSuppliersColumns: ColumnDef<Quarry>[] = [
+  {
+    id: 'name',
+    accessorFn: (row) => row.name,
+    header: ({ column }) => {
+      return <TableClientSortableHeader column={column} title="Supplier / Quarry Name" />;
+    },
+    cell: (info) => <div className="py-2">{info.getValue() as string}</div>,
+    meta: 'Supplier / Quarry Name',
+  },
+  {
+    id: 'type',
+    accessorFn: (row) => row.type,
+    header: ({ column }) => {
+      return <TableClientSortableHeader column={column} title="Type" />;
+    },
+    cell: ({ row }) => {
+      const type = row.original.type;
+      return (
+        <div className="py-2">
+          <TableBadges names={[type]} visibleCount={1} />
+        </div>
+      );
+    },
+    meta: 'Type',
+  },
+
+  {
+    id: 'email',
+    accessorFn: (row) => row.email,
+    header: ({ column }) => {
+      return (
+        <TableClientSortableHeader column={column} title="Email" />
+      );
+    },
+    cell: (info) => <div className="py-2">{info.getValue() as string}</div>,
+    meta: 'Email',
+  },
+
+  {
+    id: 'phone',
+    accessorFn: (row) => row.phone,
+    header: ({ column }) => {
+      return <TableClientSortableHeader column={column} title="Phone" />;
+    },
+    cell: (info) => <div className="py-2">{info.getValue() as string}</div>,
+    meta: 'Phone',
+  },
+
+   {
+    id: 'status',
+    accessorFn: (row) => row.status,
+    header: ({ column }) => {
+      return <TableClientSortableHeader column={column} title="Status" />;
+    },
+    cell: ({ row }) => {
+      const status = row.original.status;
+      return (
+        <div className="py-2">
+          <TableBadges names={[status]} visibleCount={1} />
+        </div>
+      );
+    },
+    meta: 'Status',
+  },
+
+  {
+    id: 'actions',
+    header: () => {
+      return <div className="text-right pr-4"></div>;
+    },
+    cell: ({ row }) => {
+      const quarry = row.original;
+      return (
+        <div className="flex justify-end pr-4">
+          <QuarryTableAction quarry={quarry} />
+        </div>
+      );
+    },
+  },
+];
