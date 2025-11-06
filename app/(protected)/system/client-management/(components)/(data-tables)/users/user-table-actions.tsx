b45f8ea06c885ea5d80c4be650eec3c30456c093
CharliePeng <charliepeng516@gmail.com>
2025-11-07 10:39:26 +1100

    update icon

--- app/(protected)/system/client-management/(components)/(data-tables)/users/user-table-actions.tsx
+++ app/(protected)/system/client-management/(components)/(data-tables)/users/user-table-actions.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as React from 'react';
-import { MoreHorizontal, Trash2 } from 'lucide-react';
+import { MoreHorizontal, Eye, Key, Delete } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -17,6 +17,18 @@ interface UserTableActionsProps {
 export function UserTableActions({ user }: UserTableActionsProps) {
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
 
+  const handleViewEdit = () => {
+    setDropdownOpen(false);
+    // TODO: Implement view/edit user functionality
+    console.log('View/Edit user:', user);
+  };
+
+  const handleResetPassword = () => {
+    setDropdownOpen(false);
+    // TODO: Implement reset password functionality
+    console.log('Reset password for user:', user);
+  };
+
   const handleDelete = () => {
     setDropdownOpen(false);
     // TODO: Implement delete user functionality
@@ -32,12 +44,22 @@ export function UserTableActions({ user }: UserTableActionsProps) {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem onClick={handleViewEdit}>
+            <Eye className="h-4 w-4 mr-2" />
+            View/Edit User
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={handleResetPassword}>
+            <Key className="h-4 w-4 mr-2" />
+            Reset Password
+          </DropdownMenuItem>
+
           <DropdownMenuItem
             onClick={handleDelete}
             className="text-destructive focus:text-destructive"
           >
-            <Trash2 className="h-4 w-4 mr-2 text-red-600" />
-            Delete User
+            <Delete className="h-4 w-4 mr-2 text-red-600" />
+            Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
