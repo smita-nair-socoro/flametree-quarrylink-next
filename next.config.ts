2a0420c747c79acbba9db1b736cef9467fe60ee6
Charlie Peng <charlie.peng@socoro.com.au>
2025-11-14 15:53:56 +1100

    update to allow server-side render to allow future server-side token validation

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   trailingSlash: true,
@@ -12,11 +11,6 @@ const nextConfig: NextConfig = {
 
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-
-  // Disable image optimization for static export
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
