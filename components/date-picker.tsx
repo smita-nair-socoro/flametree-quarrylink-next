e04ace01328c1f8c87c8b440f9488f6eab97b2e2
Charlie Peng <charlie.peng@socoro.com.au>
2025-11-21 14:35:17 +1100

    to allow user see more date

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -76,8 +76,8 @@ export function DatePicker({
           onSelect={handleDateSelect}
           disabled={disabled}
           required={required}
-          fromDate={new Date(2000, 0, 1)}
-          toDate={new Date(2050, 11, 31)}
+          startMonth={new Date(2000, 0)}
+          endMonth={new Date(2050, 11)}
         />
       </PopoverContent>
     </Popover>
