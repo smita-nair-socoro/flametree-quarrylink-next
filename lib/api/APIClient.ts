ecd3a19efff067bdf4b8d25a2ce7b1ff612e6639
Charlie Peng <charlie.peng@socoro.com.au>
2026-03-30 15:49:38 +1100

    switch back to id token

--- lib/api/APIClient.ts
+++ lib/api/APIClient.ts
@@ -179,10 +179,10 @@ export async function HttpClient<T = unknown>(
   const authUser = await getUser(); // ✅ Properly awaited
   const tenantId = await getTenantId();
 
-  if (authUser?.access_token) {
+  if (authUser?.access_token && authUser?.id_token) {
     init.headers = {
       ...init.headers,
-      Authorization: `Bearer ${authUser.access_token}`,
+      Authorization: `Bearer ${authUser.id_token}`,
       // 'access-token': authUser.access_token,
       // 'id-token': authUser.id_token,
       'X-Tenant-ID': tenantId || '',
