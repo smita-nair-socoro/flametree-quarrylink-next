e598b7bbdd48d50c74b29e7aebbdf048367a80de
unknown <jaywoo.choi@socoro.com.au>
2026-04-02 10:48:57 +1100

    done

--- lib/api/APIClient.ts
+++ lib/api/APIClient.ts
@@ -997,6 +997,11 @@ export const APIClient = {
         body: data,
       });
     },
+    deleteJobItem: (id: number) => {
+      return appClient.Delete<JobItem>(
+        `/socoro/quarrylink/api/job-items/${id}`,
+      );
+    },
   },
 
   tenants: {
