a8a882e4ea132e02a8651e41872c78985dc767d7
CharliePeng <charliepeng516@gmail.com>
2026-05-14 17:59:08 +1000

    remove delete truck

--- hooks/use-truck-actions.tsx
+++ hooks/use-truck-actions.tsx
@@ -328,7 +328,6 @@ export function useTruckActions(truckData?: TruckDTO | null) {
       allDrivers,
       assignedDriverIds,
       blockedDocketIds,
-      isNavigating,
       handleNavigate,
     ],
   );
