aa05a4b4baafda3785a20bc04197a8a36b03a977
Johnny Zhou <johnny.zhou@socoro.com.au>
2026-05-18 10:55:18 +1000

    decimal

--- lib/utils/number.ts
+++ lib/utils/number.ts
@@ -1,7 +1,7 @@
 // Number format helper functions
-export function formatNumberThousandSeparator(number: number | undefined | null, decimals = 2): string {
+export function formatNumberThousandSeparator(number: number | undefined | null): string {
   return (number ?? 0).toLocaleString('en-US', {
-    minimumFractionDigits: decimals,
-    maximumFractionDigits: decimals,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   });
 }
