66acf09af1094aa785f6d09df7ad2580ea064ddc
Johnny Zhou <johnny.zhou@socoro.com.au>
2026-05-20 16:31:35 +1000

    phone name

--- app/(protected)/customer-operations/jobs/(components)/forms/schemas/job-form-schema.ts
+++ app/(protected)/customer-operations/jobs/(components)/forms/schemas/job-form-schema.ts
@@ -1,4 +1,5 @@
 import z from 'zod';
+import { isValidPhoneNumber } from 'react-phone-number-input';
 
 const timeWithoutZoneRegex =
   /^([01]\d|2[0-3]):([0-5]\d)(?::([0-5]\d)(\.\d{1,6})?)?$/;
@@ -31,8 +32,17 @@ export type JobFormValues = z.infer<typeof JobFormSchema>;
 export const getJobFormSchema = (isEditing: boolean) => {
   if (isEditing) {
     return JobFormSchema.extend({
-      contactPersonName: z.string().min(1, { message: 'Required' }),
-      phone: z.string().min(1, { message: 'Required' }),
+      contactPersonName: z
+        .string()
+        .min(2, { message: 'Product Name must be at least 2 characters.' })
+        .max(100, { message: "Product Name can't be more than 100 characters" }),
+      phone: z
+        .string()
+        .trim()
+        .nonempty({ message: 'Phone number is required' })
+        .refine((v) => !v || isValidPhoneNumber(v), {
+          message: 'Invalid phone number',
+        }),
     });
   }
   return JobFormSchema;
