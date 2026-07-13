c5c43993a78c1ec022d912f5bcd0f04f71dcadc4
Jaywoo Choi <jaywoo.choi@socoro.com.au>
2026-07-13 11:39:01 +1000

    removed console log

--- app/(protected)/customer-operations/customers/(components)/forms/add-customer-attachment-dialog.tsx
+++ app/(protected)/customer-operations/customers/(components)/forms/add-customer-attachment-dialog.tsx
@@ -69,7 +69,6 @@ export function AddCustomerAttachmentDialog({
       return;
     }
 
-    console.log('Add attachment:', { customerId, ...result.data });
 
     // TODO: wire create API when available using customerId and result.data
 
