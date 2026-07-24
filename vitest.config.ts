9ca2c46f9c2f1f68eb3bf0b42e977edc3f292a69
Charlie Peng <charlie.peng@socoro.com.au>
2026-07-24 15:23:09 +1000

    add converage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json-summary'],
+    },
   },
   resolve: {
     alias: {
