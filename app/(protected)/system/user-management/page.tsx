cff7bc98675930680ee65e7c343f58e3c37cb1f3
Charlie Peng <charlie.peng@socoro.com.au>
2026-08-12 15:39:16 +1000

    only Super admin allow to go to fee recovery

--- app/(protected)/system/user-management/page.tsx
+++ app/(protected)/system/user-management/page.tsx
@@ -66,7 +66,7 @@ export default function UserRolesPage() {
           },
         ]
       : []),
-    ...(isSuperAdmin || isAdmin
+    ...(isSuperAdmin
       ? [
           {
             name: 'Fee Recovery',
